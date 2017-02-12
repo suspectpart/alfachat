f368aa3e6451861af97f3e1bf7bcd12c6b31cef0
Horst Schneider <horst.schneider@posteo.de>
2017-02-12 14:04:49 +0100

    Move more chat code to separate module

--- alfachat.py
+++ alfachat.py
@@ -1,5 +1,37 @@
+import config
+import json
+import os
 import requests
+from datetime import datetime
 
 def send_sms_to(sms_config, number, text):
     url = "https://www.smsout.de/client/sendsms.php?Username={0}&Password={1}&SMSTo={2}&SMSType=V1&SMSText={3}"
     return requests.get(url.format(sms_config[0], sms_config[1], number, text))
+
+class MessageLine(object):
+    def __init__(self, user, message, color, timestamp = None, visible_to = []):
+        self.user = user
+        self.message = message
+        self.color = color
+        self.timestamp = timestamp if timestamp else datetime.now()
+        self.visible_to = visible_to
+        
+    def __str__(self):
+        return MessageEncoder().encode(self)
+
+class MessageEncoder(json.JSONEncoder):
+    def default(self, message):
+        if isinstance(message, MessageLine):
+            return {"user": message.user, "message": message.message, "color": message.color, "timestamp": str(message.timestamp), "visible_to": message.visible_to}
+
+        return json.JSONEncoder.default(self, obj)    
+    
+    def decode(message, obj):
+        return MessageLine(obj["user"], obj["message"], obj["color"], obj["timestamp"], obj["visible_to"])
+
+def get_appointments():
+    if os.path.isfile(config.appointments_path):
+        with open(config.appointments_path, 'r') as f:
+            return f.read().replace("\n", "   ")
+    else:
+        return "Keine Termine."
--- chat.py
+++ chat.py
@@ -5,9 +5,7 @@ import json
 import logging
 import os
 import re
-import requests
 import uuid
-from datetime import datetime
 from flask import abort
 from flask import escape
 from flask import Flask
@@ -23,37 +21,6 @@ handler = logging.FileHandler('access.log')
 logger.addHandler(handler)
 app.logger.addHandler(handler)
 
-#def send_sms_to(number, text):
-#    return requests.get("https://www.smsout.de/client/sendsms.php?Username={0}&Password={1}&SMSTo={2}&SMSType=V1&SMSText={3}".format(config.sms_config[0], config.sms_config[1], number, text))
-    
-class MessageLine(object):
-    def __init__(self, user, message, color, timestamp = None, visible_to = []):
-        self.user = user
-        self.message = message
-        self.color = color
-        self.timestamp = timestamp if timestamp else datetime.now()
-        self.visible_to = visible_to
-        
-    def __str__(self):
-        return MessageEncoder().encode(self)
-
-class MessageEncoder(json.JSONEncoder):
-    def default(self, message):
-        if isinstance(message, MessageLine):
-            return {"user": message.user, "message": message.message, "color": message.color, "timestamp": str(message.timestamp), "visible_to": message.visible_to}
-
-        return json.JSONEncoder.default(self, obj)    
-    
-    def decode(message, obj):
-        return MessageLine(obj["user"], obj["message"], obj["color"], obj["timestamp"], obj["visible_to"])
-
-def get_appointments():
-    if os.path.isfile(config.appointments_path):
-        with open(config.appointments_path, 'r') as f:
-            return f.read().replace("\n", "   ")
-    else:
-        return "Keine Termine."
-        
 
 @app.route('/robots.txt')
 def static_from_root():
@@ -67,13 +34,7 @@ def hello(token):
         abort(404)
 
     if request.method == 'POST':
-        message = MessageLine(user, escape(request.form['message']), color)
-        
-        if message.message =="::cls":
-            with open("chat.log", 'w+') as f:
-                f.write("")
-                render_template('alfachat.html', user=user)
-
+        message = ac.MessageLine(user, escape(request.form['message']), color)
         
         with open("chat.log", 'a+') as f:
             for u in config.users.values():
@@ -87,7 +48,7 @@ def hello(token):
             f.write(str(message) + "\n")
             
             if message.message == "@bot termine":
-                f.write(str(MessageLine("alfabot", get_appointments(), "gray")) + "\n")
+                f.write(str(ac.MessageLine("alfabot", ac.get_appointments(), "gray")) + "\n")
    
     return render_template('alfachat.html', user=user, user_id=uuid.UUID(token))
 
@@ -105,7 +66,7 @@ def messages(user_id):
             log = f.read().split("\n")
             for line in log:
                 if line.strip():
-                    msg_json = MessageEncoder().decode(json.loads(line))
+                    msg_json = ac.MessageEncoder().decode(json.loads(line))
                     msg_json.message = re.sub(pattern, r'<a href="\g<1>" target="_blank">\g<1></a>', msg_json.message)
                     messages.append(msg_json)
         
