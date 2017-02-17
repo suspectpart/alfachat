c0fc45258dd1e01b46516d8cd882e4b12279d95c
Horst Schneider <horst.schneider@posteo.de>
2017-02-17 09:59:24 +0000

    Add url regex that also matches the fragment part of an URL

--- alfachat.py
+++ alfachat.py
@@ -22,7 +22,7 @@ def write_chat(message_lines):
 
 
 def read_chat():
-    pattern = re.compile(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)')
+    pattern = re.compile(r'(https?:[\/\/|\\\\]+([\w\d:#@%\/;$()~_?\+-=\\\.&](#!)?)*)')
 
     messages = []
 
